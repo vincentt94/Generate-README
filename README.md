2c5cf03960d3555875df69cfda1300440c8309eb
vincentt94 <vincent.h.thao@gmail.com>
2025-01-19 20:59:49 -0600

    Add files via upload
    Updating format for index & helper

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
-# Generate-README
-This app will generate a README through a series of prompts via node.js
+# Create a README.md through Node.js
+  [![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC)  
+
+  ## Description
+  Every developer needs a  useful and meaningful README whenever they are deploying a new project.  Creating a README can be a tedious and time consuming task.  In order to save time for a developer, I was able to generate a boiler-plate README through a series of questions utilizing node.js.  This project helped me further my understanding of node.js as well as deepen my grasp of functions, parameters within functions, and syntax significance.  
+
+## Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Guidelines](#guidelines)
+- [Tests](#tests)
+- [Questions](#questions)
+
+  ## Installation
+  In order to install this application a  user would first have to create two javascript files.  For the purposes of this example, we will create an index.js and a helper.js.  After creating both javascript files, the user will have to initialize a Node Packaging Manager.   To do so, the user can open their terminal and type in the command 'npm init -y' .  This will result in a package.json being created.  Navigate to the that package.json and after the quotation marks behind "type":  insert a ' , ' then hit enter and paste ' "type": "module" ' .  Doing this enables ES 6 modules.  Once we finish this, we will go back to the terminal and install the inquirer package.  Navigate to the terminal again and type 'npm i inquirer' . Now the user can paste the index.js code into the javascript file named 'index.js' and the helper.js code into the 'helper.js' file.  This step is very important.
+
+  ## Usage
+  For help with the instructions, please watch this link https://drive.google.com/file/d/1868Jl4z6kT9q6PVTIT-hK3ndqktK0Knd/preview
+
+  ## Licenses
+  https://opensource.org/licenses/ISC
+
+  ## Guidelines 
+  No contributions necessary for this project. 
+
+  ## Tests
+  Once everything is installed correctly, go into your terminal and type the command 'node index'.  A series of questions will then prompt you so you can generate your own custom README
+
+  ## Questions
+  If you have any questions, you can reach me at vincent.h.thao@gmail.com or visit my GitHub profile at https://github.com/vincentt94/
+
+
--- index.js
+++ index.js
@@ -8,71 +8,71 @@ import fs from 'fs';
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
     // const userInfo = ({title, describe, install, usage, guide, instruct, github, email, license}) => generateMarkdown ;  
-    
+
     fs.writeFile(fileName, generateMarkdown(data), (err) =>
-    err ? console.log(err) : console.log('Successfully created README.md')
-)
+        err ? console.log(err) : console.log('Successfully created README.md')
+    )
 };
 
 // TODO: Create a function to initialize app
 function init() {
     inquirer
-    .prompt([
-    {
-        type: 'input',
-        message: 'What is your project title?',
-        name: 'title',
-    },
-    {
-        type: 'input',
-        message: 'What is the description of the project?',
-        name: 'describe',
-    },
-    {
-        type: 'input',
-        message: 'What are the installation instructions?',
-        name: 'install',
-    },
-    {
-        type: 'input',
-        message: 'What is the usage information?',
-        name: 'usage',
-    },
-    {
-        type: 'input',
-        message: 'What are the contribution guidelines?',
-        name: 'guide',
-    },
-    {
-        type: 'input',
-        message: 'What are the test instructions?',
-        name: 'instruct',
-    },
-    {
-        type: 'input',
-        message: 'What is your github username?',
-        name: 'github',
-    },
-    {
-        type: 'input',
-        message: 'What is your email address?',
-        name: 'email',
-    },
-    {
-        message: 'Please select a license for your project',
-        choices: ['MIT', 'Apache 2.0', 'GPL 3.0', 'BSD 3', 'ISC', 'Mozilla'],
-        type: 'list',
-        name: 'license',
-    },
-])
-.then((answers) => {
+        .prompt([
+            {
+                type: 'input',
+                message: 'What is the title of your project?',
+                name: 'title',
+            },
+            {
+                type: 'input',
+                message: 'What is the description of the project?  You can include your motivation, why you built the project, what problem(s) it may solve, and what you leanred along the way.',
+                name: 'describe',
+            },
+            {
+                type: 'input',
+                message: 'What are the installation instructions? Please include the steps required to install the project if necessary.',
+                name: 'install',
+            },
+            {
+                type: 'input',
+                message: 'What is the usage information? Please provide instructions and or examples for use.',
+                name: 'usage',
+            },
+            {
+                type: 'input',
+                message: 'What are the contribution guidelines?',
+                name: 'guide',
+            },
+            {
+                type: 'input',
+                message: 'What are the test instructions?',
+                name: 'instruct',
+            },
+            {
+                type: 'input',
+                message: 'What is your github username?',
+                name: 'github',
+            },
+            {
+                type: 'input',
+                message: 'What is your email address?',
+                name: 'email',
+            },
+            {
+                message: 'Please select a license for your project',
+                choices: ['MIT', 'Apache 2.0', 'GPL 3.0', 'BSD 3', 'ISC', 'Mozilla'],
+                type: 'list',
+                name: 'license',
+            },
+        ])
+        .then((answers) => {
 
-    
-    // write answers to file
-    writeToFile('README.md', answers);
-})
 
- }
+            // write answers to file
+            writeToFile('README.md', answers);
+        })
+
+}
 
 // Function call to initialize app
 init();
